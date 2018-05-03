306abe349d8114188933bf2c047ef9baea6aa744
Dzokaredevil <dima.voytovich@mail.ru>
2018-05-03 03:48:26 +0300

    JB task Timeline Visualization (Time Series + Events)

---
+++ LeaveEnter/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+        }
+    }
+}
